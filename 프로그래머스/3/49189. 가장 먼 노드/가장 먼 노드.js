8546c4dd3ac6e3724ebc95704174fedcfd599036
seungwon <97tmddnjs@naver.com>
2025-05-27 11:08:01 +0900

    [level 3] Title: 가장 먼 노드, Time: 44.29 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -10,53 +10,14 @@ function solution(n, edge) {
         graph[x].push(y)
         graph[y].push(x)
     })
-    
-    const result = {}
-    let maxD = 0
+
     let answer = 0
-    
-//     const bfs = () => {
-//         const visited = [1]
-//         let needVisit = []
-        
-//         // 각 원소에 노드와 거리 저장
-//         needVisit.push({
-//             currentNode : 1,
-//             d : 0
-//         })
-        
-//         while ( needVisit.length> 0) {
-//             const {currentNode, d} = needVisit.shift()
-            
-//             // 다음 거리에 있는 노드리스트
-//             const nextNodes = graph[currentNode]
-//             nextNodes.forEach((el)=>{
-//                 if(!visited.includes(el)) {
-                    
-//                     // 최대거리에 있는 노드 체크
-//                     if(maxD < d){
-//                         maxD = d
-//                         answer = 1
-//                     }
-//                     else if (maxD === d){
-//                         answer += 1
-//                     }
-                    
-//                     // 다음 순회할 노드 추가
-//                     visited.push(Number(el))
-//                     needVisit = [...needVisit, {
-//                         currentNode : el,
-//                         d : d+1
-//                     }]
-//                 }
-//             })
-//         }
-//     }
     
     const dist = { 1:0 }
     
     const bfs = () => {
-        const visited = [1]
+        const visited = new Set();
+        visited.add(1);
         let needVisit = []
         
         // 각 원소에 노드와 거리 저장
@@ -68,13 +29,13 @@ function solution(n, edge) {
             // 다음 거리에 있는 노드리스트
             const nextNodes = graph[node]
             nextNodes.forEach((el)=>{
-                if(!visited.includes(el)) {
+                if(!visited.has(el)) {
                     
                     dist[el] = dist[node]+1
                     
                     // 다음 순회할 노드 추가
-                    visited.push(el)
-                    needVisit = [...needVisit,el]
+                    visited.add(el)
+                    needVisit.push(el)
                 }
             })
         }
