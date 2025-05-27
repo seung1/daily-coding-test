8585dd556041ee9e9bc4f5a25a4236450c989ce7
seungwon <97tmddnjs@naver.com>
2025-05-27 11:00:31 +0900

    [level 3] Title: 가장 먼 노드, Time: 1936.44 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -15,45 +15,81 @@ function solution(n, edge) {
     let maxD = 0
     let answer = 0
     
+//     const bfs = () => {
+//         const visited = [1]
+//         let needVisit = []
+        
+//         // 각 원소에 노드와 거리 저장
+//         needVisit.push({
+//             currentNode : 1,
+//             d : 0
+//         })
+        
+//         while ( needVisit.length> 0) {
+//             const {currentNode, d} = needVisit.shift()
+            
+//             // 다음 거리에 있는 노드리스트
+//             const nextNodes = graph[currentNode]
+//             nextNodes.forEach((el)=>{
+//                 if(!visited.includes(el)) {
+                    
+//                     // 최대거리에 있는 노드 체크
+//                     if(maxD < d){
+//                         maxD = d
+//                         answer = 1
+//                     }
+//                     else if (maxD === d){
+//                         answer += 1
+//                     }
+                    
+//                     // 다음 순회할 노드 추가
+//                     visited.push(Number(el))
+//                     needVisit = [...needVisit, {
+//                         currentNode : el,
+//                         d : d+1
+//                     }]
+//                 }
+//             })
+//         }
+//     }
+    
+    const dist = { 1:0 }
+    
     const bfs = () => {
         const visited = [1]
         let needVisit = []
         
         // 각 원소에 노드와 거리 저장
-        needVisit.push({
-            currentNode : 1,
-            d : 0
-        })
+        needVisit.push(1)
         
         while ( needVisit.length> 0) {
-            const {currentNode, d} = needVisit.shift()
+            const node = needVisit.shift()
             
             // 다음 거리에 있는 노드리스트
-            const nextNodes = graph[currentNode]
+            const nextNodes = graph[node]
             nextNodes.forEach((el)=>{
                 if(!visited.includes(el)) {
                     
-                    // 최대거리에 있는 노드 체크
-                    if(maxD < d){
-                        maxD = d
-                        answer = 1
-                    }
-                    else if (maxD === d){
-                        answer += 1
-                    }
+                    dist[el] = dist[node]+1
                     
                     // 다음 순회할 노드 추가
-                    visited.push(Number(el))
-                    needVisit = [...needVisit, {
-                        currentNode : el,
-                        d : d+1
-                    }]
+                    visited.push(el)
+                    needVisit = [...needVisit,el]
                 }
             })
         }
     }
     
     bfs()
+    
+    const valueList = Object.values(dist)
+    const maxValue = Math.max(...valueList)
+    
+    valueList.forEach((el)=>{
+        if(el === maxValue){
+            answer += 1
+        }
+    })
     
     return answer
 }
