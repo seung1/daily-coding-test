981f376343345b30c6aeffe2bf9d71b48c490642
seungwon <97tmddnjs@naver.com>
2025-07-24 14:45:44 +0900

    [level 1] Title: 예산, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -2,15 +2,6 @@ function solution(d, budget) {
     var answer = 0;
     
     const sorted = d.sort((a,b)=>a-b)
-    
-    while(budget > 0){
-        if(budget >= sorted[answer] ){
-            budget -= sorted[answer]
-            answer += 1
-        }
-        else {
-            break
-        }
-    }
+    console.log(sorted)
     return answer;
 }
