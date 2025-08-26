7a2725d6887240a92e3dedbd47b2e80aacfd227d
seungwon <97tmddnjs@naver.com>
2025-08-27 05:21:15 +0900

    [level 3] Title: 봉인된 주문, Time: 502.77 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/README.md
+++ 프로그래머스/3/389481. 봉인된 주문/README.md
@@ -1,10 +1,10 @@
 # [level 3] 봉인된 주문 - 389481 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389481) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389481#) 
 
 ### 성능 요약
 
-메모리: 117 MB, 시간: 2270.66 ms
+메모리: 117 MB, 시간: 502.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 27일 04:45:28
+2025년 08월 27일 05:21:13
 
 ### 문제 설명
 
--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.js
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.js
@@ -1,92 +1,40 @@
 function solution(n, bans) {
-    var answer = '';
-    
-    // bans을 순회하면서 
-    // 각 문자열을 숫자로 바꾸는 함수
-    // n보다 작을 경우는 n+=1
-    // 최종 n에 맞는 문자를 리턴
-    bans = bans.sort((a, b) => {
-  if (a.length !== b.length) {
-    return a.length - b.length; // 1차: 길이 짧은 순
-  }
-  return a.localeCompare(b);   // 2차: 사전식 순
-});
-    
-    
-    const alphabet = [0,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u",
-                     "v","w","x","y","z"]
-    
+    // 문자를 몇번째인지 바꾸는 함수
     const strToNum = (str) =>{
         const list  = str.split("")
         let result = 0
         list.forEach((el, index)=>{ 
-            const num = alphabet.findIndex(e=>e===el)
+            const num = el.charCodeAt(0) - 96
             result+= num * (26 ** (list.length - 1 - index))
             return num
         })
         
         return result
     }
-    const alToNum1 = {
-        1:"a",
-        2:"b",
-        3:"c",
-        4:"d",
-        5:"e",
-        6:"f",
-        7:"g",
-        8:"h",
-        9:"i",
-        a:"j",
-        b:"k",
-        c:"l",
-        d:"m",
-        e:"n",
-        f:"o",
-        g:"p",
-        h:"q",
-        i:"r",
-        j:"s",
-        k:"t",
-        l:"u",
-        m:"v",
-        n:"w",
-        o:"x",
-        p:"y",
-        q:"z"
-    }
-    const keyList = Object.keys(alToNum1)
-    const valueList = Object.values(alToNum1)
     
+    // 문자를 먼저 숫자로 바꾸고 미리 정렬 -> 숫자정렬이 더 빠르다
+    bans = bans.map((el)=>strToNum(el))
+    bans = bans.sort((a,b)=>a-b)
+    
+    // 숫자를 a-z 진법으로 바꾸는 함수
     const numToStr = (num) => {
-        const str = num.toString(26)
-        let list = str.split("")
-        if(list.includes("0")){
-            for(let i=0;i<list.length-1; i++){
-                if(list[i+1]==="0"){
-                    const index = keyList.findIndex(e=>e===list[i])
-                    list[i] = keyList[index-1]
-                    list[i+1] = "q"
-                }
-            }    
+        let out = "";
+        while (num > 0) {
+            const r = (num - 1) % 26;               // 0..25
+            out = String.fromCharCode(97 + r) + out; // 97='a'
+            num = Math.floor((num - 1) / 26);
         }
-        
-        list = list.map((el,index)=>{
-                return alToNum1[el]
-            
-        })
-        return list.join("")
+        return out;
     }
     
     let k = n
     let count = 0
-    bans = bans.map((el)=>{
-        if(strToNum(el)<=k){
-            k+=1
-        }
-        return strToNum(el)
-    })
     
+    // 조건에 맞을경우 +1 아닐경우 빠져나오기
+    for(const el of bans){
+        if((el)<=k) k+=1
+        else break
+    }
     
     return numToStr(k)
 }
