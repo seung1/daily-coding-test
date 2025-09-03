43198828f59afed5a40741da431028f5fed039a4
seungwon <97tmddnjs@naver.com>
2025-09-04 00:34:54 +0900

    [level 3] Title: 스티커 모으기(2), Time: 9.22 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -2,16 +2,19 @@ function solution(sticker) {
     const dp1 = Array(sticker.length-1).fill(0)
     const dp2 = Array(sticker.length-1).fill(0)
     
+    // 3개 이하인 경우
     if(sticker.length < 4){
         return Math.max(...sticker)
     }
     
+    // 0번째꺼 선택하는 경우
     dp1[0] = sticker[0]
     dp1[1] = Math.max(sticker[0],sticker[1])
     for(let i =2;i<sticker.length-1;i++){
         dp1[i] = Math.max(dp1[i-1],dp1[i-2]+sticker[i])
     }
     
+    // 1번째꺼 선택하는 경우
     dp2[0] = 0
     dp2[1] = sticker[1]
     dp2[2] = Math.max(sticker[1],sticker[2])
