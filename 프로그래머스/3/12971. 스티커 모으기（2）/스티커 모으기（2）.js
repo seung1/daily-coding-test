d71866b621510a3afa7a731993c8cfa97b86c9b1
seungwon <97tmddnjs@naver.com>
2025-09-04 00:34:06 +0900

    [level 3] Title: 스티커 모으기(2), Time: 9.18 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -6,19 +6,18 @@ function solution(sticker) {
         return Math.max(...sticker)
     }
     
-    // 0번째꺼 선택하는 경우
     dp1[0] = sticker[0]
-    dp1[1] = Math.max(sticker[0], sticker[1])
-    for(let i =2; i<sticker.length-1;i++){
-        dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i]);
+    dp1[1] = Math.max(sticker[0],sticker[1])
+    for(let i =2;i<sticker.length-1;i++){
+        dp1[i] = Math.max(dp1[i-1],dp1[i-2]+sticker[i])
     }
     
-    // 1번째꺼 선택하는 경우
-    dp2[0] = sticker[1]
-    dp2[1] = Math.max(sticker[1], sticker[2])
-    for(let i =2; i<sticker.length-1;i++){
-        dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i+1]);
+    dp2[0] = 0
+    dp2[1] = sticker[1]
+    dp2[2] = Math.max(sticker[1],sticker[2])
+    for(let i =3;i<sticker.length;i++){
+        dp2[i] = Math.max(dp2[i-1],dp2[i-2]+sticker[i])
     }
-    
-    return Math.max(dp1[sticker.length-2], dp2[sticker.length-2])
+
+    return Math.max(dp1[dp1.length-1],dp2[dp2.length-1])
 }
