0de0a4a6a0eab49fe093e3f5027d7f66a29c4527
seungwon <97tmddnjs@naver.com>
2025-09-15 08:28:56 +0900

    [level 3] Title: 숫자 게임, Time: 87.49 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
@@ -1,26 +1,20 @@
 function solution(A, B) {
-    // 순서는 중요하지 않다.
-    // 둘다 오름차순으로 정렬
+    var answer = 0;
     
-    // A와 B를 동시에 순회하면서
-    // B가 이기는지 체크
-    // 만약 B가 진다면, 이길때까지 넘어가기
-    // B의 끝에 다다를때까지 반복하고 ai 리턴
-    
-    let sortedA = A.sort((a,b)=>a-b)
-    let sortedB = B.sort((a,b)=>a-b)
+    // a를 정렬하고
+    const sortedA = A.sort((a,b)=>a-b)
+    const sortedB = B.sort((a,b)=>a-b)
     
     let ai = 0
     let bi = 0
-    while(bi<B.length){
-        if(A[ai]<B[bi]){
+    // a의 하나씩 포인터 삼아서 b를 순회하기
+    while(bi<sortedA.length){
+        if(sortedA[ai]<sortedB[bi]){
+            answer+=1
             ai+=1
-            bi+=1
         }
-        else {
-            bi += 1
-        }
+        bi+=1
     }
     
-    return ai;
+    return answer;
 }
